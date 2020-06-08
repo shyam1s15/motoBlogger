d68610bfa02523e9fe822a720d237b0ca8581262
shyam1s15 <shyam1ss15@gmail.com>
2020-06-08 22:38:29 +0530

    sign up confirm

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
  *      sendMail($email,$pass) is function used when signup
  *      getSignUp() function is just an brige for redirect to target page
  *      postSignUp() function will have actual all logics for bad sign up,
- *                  successful sign up,it will  
+ *                  successful sign up,it will
  *      checkMail() function will be used when user confirms from his mail for signups
  *      getLogIn() function is just an brige
  *      postLogIn() function have actual login and will redirect back if bad details or
@@ -29,26 +29,26 @@ class loginController extends Controller
     //
     public function sendMail($email,$pass){
         $hashpass = \Hash::make($pass);
-        
+
         $details = [
             'title' => 'please Sign Up using below below mail',
             'body' => 'Dear User please click below link to confirm your registation,
                         if you believe this is not done by you please ignore it thankyou
                         for your time and attention',
-            'link'=> 'http://localhost:8000/checkMail?mail='. $email . '&pass=' . $hashpass ,
+            'link'=> '{{ env("APP_URL") }}/checkMail?mail='. $email . '&pass=' . $hashpass ,
         ];
-        
-        \Mail::to($email)->send(new \App\Mail\SendMailable($details));    
+
+        \Mail::to($email)->send(new \App\Mail\SendMailable($details));
     }
     // this function will only called for entry well no authenication is required here.
-    
+
     public function getSignUp(){
         return view('layouts.logups.sign_up');
     }
 
     // in this below function we need to authenticate as well database queries must be checked here
 
-    public function postSignUp(){        
+    public function postSignUp(){
         $users2 = \DB::table('students')->where('email','=', $_POST['email'])->get();
 
         // return view('layouts.logups.test', ['users' => $users2]);
@@ -56,7 +56,7 @@ class loginController extends Controller
             return redirect()->back()->with('user_exists','just dont do that')->withInput();
         }
         // else section will not be executed as we are returned allready
-        
+
         $ans = \DB::table('tempStudents')->insert([
             'email'=>$_POST['email'],
             'password'=>$_POST['password'],
@@ -65,21 +65,21 @@ class loginController extends Controller
             'city'=>$_POST['city'],
             'state'=>$_POST['state'],
             'zipcode'=>$_POST['zipcode']
-        ]);   
-        
+        ]);
+
         $this->sendMail($_POST['email'],$_POST['password']);
         return view('layouts.logups.result_of_signup_login',['result'=>'Pre Signed Up']);
         // we must redirect user to welcome page that page is still not created...
     }
 
     public function checkMail(){
-        
+
         $email = $_GET['mail'];
         $pass = $_GET['pass'];
 
         $ans = \DB::table('tempStudents')->select('email','password')->where('email','=',
         $email)->get()->first();
-        
+
         if( strcmp($pass,\Hash::make($ans->password)) ){
 
             // most hutyapa logic used here....simple but hard to implement when finding
@@ -100,13 +100,13 @@ class loginController extends Controller
                 'city'=>$val->city,
                 'state'=>$val->state,
                 'zipcode'=>$val->zipcode,
-            ]);      
+            ]);
         }
         // return $ans->password;
         // return strcmp($pass,\Hash::make($ans->password))?"true":"false";
         // return \Hash::check($ans->password,$pass)?"true":"false";
         return view('layouts.logups.result_of_signup_login',['result'=>'Signed Up']);
-    } 
+    }
     // function ended for sign ups lets focus on logins
     public function getLogIn(){
         return view('layouts.logups.login_users');
@@ -146,9 +146,9 @@ class loginController extends Controller
         $details = [
             'title' => 'Forgot password',
             'body' => 'be my friend u will be in profit, be my enemy i will get much profit',
-            'link'=> 'http://localhost:8000/forgotPassMail?mail='. $email . '&pass=' . $hashEmail ,
+            'link'=> '{{ env("APP_URL") }}/forgotPassMail?mail='. $email . '&pass=' . $hashEmail ,
         ];
-        \Mail::to($email)->send(new \App\Mail\SendMailable($details)); 
+        \Mail::to($email)->send(new \App\Mail\SendMailable($details));
         return view('layouts.logups.result_of_signup_login',['result'=>"checkMail for pass"]);
     }
 
@@ -167,7 +167,7 @@ class loginController extends Controller
         $ans = \DB::table('students')->where([
             ['email','=', $_POST['mail']],
         ])->update(['password'=>$_POST['password']]);
-        
+
         return view('layouts.logups.result_of_signup_login',['result'=>'success_pass_change']);
     }
 }
